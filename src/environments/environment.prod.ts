324618af09e7047c08050787e49d5dffbf14f5d7
maithili030690 <maithili.shimpankar@gmail.com>
2025-03-14 18:58:45 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiConFig:{
-    baseUrl:'http://api.themoviedb.org/3',
+    baseUrl:'https://api.themoviedb.org/3',
     apiKey:'72a72a042eade3d6a642fac4abfae6cd',
     defaultLanguage:'en-US'
   }
